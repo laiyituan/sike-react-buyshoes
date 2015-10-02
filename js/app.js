30f04932511d033125a160da2118929cd97ba5a6
Botao <wxt2005@gmail.com>
2015-10-02 21:58:44 +0800

    Build basic layout

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+function makeCartScrollNicely(){
+    var cart = document.querySelector('.cart__content'),
+        content = document.querySelector('.site__content');
+
+    Ps.initialize(cart);
+    Ps.initialize(content);
+
+}
+
 window.onload = function() {
   console.log("page loaded");
-}
+  makeCartScrollNicely();
+};
